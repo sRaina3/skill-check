695595c4bfa97e2c4b7115585b78927803fd1bf5
Saransh Raina <saranshraina1@gmail.com>
2023-05-08 18:28:14 -0500

    Index game display

--- src/IndexMemory.js
+++ src/IndexMemory.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './IndexMemory.css';
 
 const IndexMemory = () => {
+  const [correctSquares, setCorrect] = useState([])
+  const [userSquares, setUser] = useState([])
   const navigate = useNavigate();
 
   const handleGoBack = () => {
     navigate('/');
   };
 
+  const handleClick = () => {
+    setUser(userSquares.concat(1))
+  }
+
   console.log("here")
   return (
     <div>
       <h1 className="title">Sequence Memory</h1>
-      <button className="home-button" onClick={handleGoBack}>Home</button>
+      <div className='index-button-container'>
+        <button className="home-button" onClick={handleGoBack}>Home</button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+        <button className='index-button' onClick={handleClick}></button>
+      </div>
     </div>
   );
 };
