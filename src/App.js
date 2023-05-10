25ca96995728276afc6d15b757c567367400341d
Saransh Raina <saranshraina1@gmail.com>
2023-05-09 20:35:30 -0500

    rename

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const navigate = useNavigate();
 
   const handleGameButtonClick = () => {
-    navigate('/IndexMemory');
+    navigate('/SequenceMemory');
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
-import IndexMemory from './IndexMemory';
+import SequenceMemory from './SequenceMemory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
       <Route path="*" element={<App />} />
-      <Route path="/IndexMemory" element={<IndexMemory />} />
+      <Route path="/SequenceMemory" element={<SequenceMemory />} />
     </Routes>
   </Router>
 );
