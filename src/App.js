e9a74834ef393512cc9ac07f7baf0a76572cb659
Saransh Raina <saranshraina1@gmail.com>
2023-05-11 21:04:20 -0500

    button hover

--- src/App.js
+++ src/App.js
@@ -8,10 +8,23 @@ const App = () => {
     navigate('/SequenceMemory');
   };
 
+  const handleNormalMode = () => {
+
+  }
+
+  const handleChallengeMode = () => {
+
+  }
+
   return (
     <div className="title-screen">
       <h1 className="title-text">Choose a Game</h1>
-      <button className="game-button" onClick={handleGameButtonClick}>Sequence Memory</button> 
+      <button className="game-button" onClick={handleGameButtonClick}>Sequence Memory</button>
+      <div>
+        <h1 className="title-text">Select Mode</h1>
+        <button className="mode-button normal" onClick={handleNormalMode}>Normal</button>
+        <button className="mode-button challenge" onClick={handleChallengeMode}>Challenge</button>
+      </div>
     </div>
   )
 }
