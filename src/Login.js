50179762351a8946fa219c87ac8ecb82cf101182
Saransh Raina <saranshraina1@gmail.com>
2023-05-16 19:38:22 -0500

    show display

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+
 const Login = () => {
+  const [username, setUser] = useState('')
+  const [password, setPass] = useState('')
+
+  const addLogin = () => {
+
+  }
+
+  const usernameUpdate = (e) => {
+    setUser(e.target.value)
+  }
+
+  const passwordUpdate = (e) => {
+    setPass(e.target.value)
+  }
+
   return (
-    <div>
-      <hi className='title'>log in attempt</hi>
+    <div className='title-text'>
+    <form onSubmit={addLogin}>
+      <div>
+        <div>Enter Username: <input value={username} onChange={usernameUpdate}/></div>
+        <div>Enter Password: <input value={password} onChange={passwordUpdate}/></div>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
     </div>
   )
 }
