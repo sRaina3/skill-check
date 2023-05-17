093b94d998ba8da1f34b911297f01b4614cc9ad1
Saransh Raina <saranshraina1@gmail.com>
2023-05-17 16:03:12 -0500

    done

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 const getUsers = () => {
   const request = axios.get('https://sraina3.github.io/Logins/db.json')
-  return request.then(respone => respone.data)
+  return request.then(respone => respone.data.logins)
 }
 
 const addUser = (newUser) => {
   const request = axios.post('https://sraina3.github.io/Logins/db.json', newUser)
-  return request.then(response => response.data)
+  return request.then(response => response.data.logins)
 }
 
 const userService = {getUsers, addUser}
