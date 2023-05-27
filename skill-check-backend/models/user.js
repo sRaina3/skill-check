18576198d74103146bb71be345a95bdd0f3a48f6
Saransh Raina <saranshraina1@gmail.com>
2023-05-27 16:35:45 -0500

    backend added support for word frequency

--- skill-check-backend/models/user.js
+++ skill-check-backend/models/user.js
@@ -21,7 +21,8 @@ const userSchema = new mongoose.Schema({
   seqCScore: Number,
   scramNScore: Number,
   scramCScore: Number,
-  skillCoins: Number
+  skillCoins: Number,
+  wordfreqNScore: Number
 })
 
 userSchema.set('toJSON', {
